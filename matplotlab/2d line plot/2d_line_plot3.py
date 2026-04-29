759a4c0b30c6e57a07b303755c0f7736e0b959b9
Himanshu Machra <himanshumachra25@gmail.com>
2026-04-29 23:24:06 +0530

    a new ploting grapgh in the matplotlib

--- matplotlab/2d line plot/2d_line_plot3.py
+++ matplotlab/2d line plot/2d_line_plot3.py
@@ -1,8 +1,7 @@
 import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt 
-import seaborn as sas 
-
+import seaborn as sas
 bt = pd.read_csv("sharma-kohli.csv")
 plt.plot(bt['index'],bt['V Kohli'],color='grey',linewidth=2,marker='o',markersize=5,label='viratcbvxxcbxcv')
 plt.plot(bt['index'],bt['RG Sharma'],color='red',linewidth=2,marker='*',markersize=5,label='sharma')
