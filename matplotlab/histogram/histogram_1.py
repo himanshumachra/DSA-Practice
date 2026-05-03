a52aa75bc88850c74839962186fd740c017e4642
Himanshu Machra <himanshumachra25@gmail.com>
2026-05-03 22:45:36 +0530

    a new stuff in matplotlib histogram

--- matplotlab/histogram/histogram_1.py
+++ matplotlab/histogram/histogram_1.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np 
 
 dt = pd.read_csv('vk.csv')
-plt.hist(dt["match_id"],bins=[20,50,60,80,90,100,130],label="virat",color="blue",binsize=2)
+plt.hist(dt["match_id"],bins=[20,50,60,80,90,100,130],log=True,label="virat",color="blue",rwidth=0.5)
 plt.legend()
 plt.show()
 print(dt)
